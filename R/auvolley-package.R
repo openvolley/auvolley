8eb5d55a39c15b86e4ac80dba2062c6e9974cb13
Ben Raymond <ben@theraymonds.org>
2021-03-03 16:14:51 +1100

    fix imports v0.0.3

--- R/auvolley-package.R
+++ R/auvolley-package.R
@@ -5,8 +5,9 @@
 #' @name auvolley
 #' @docType package
 #' @importFrom assertthat assert_that is.flag is.string
-#' @importFrom dplyr `%>%` bind_rows case_when desc distinct group_by lag lead left_join mutate n row_number ungroup
+#' @importFrom dplyr `%>%` bind_rows case_when count desc distinct group_by lag lead left_join mutate n row_number ungroup
 #' @importFrom rlang .data
+#' @importFrom stats setNames
 #' @importFrom utils packageVersion
 
 NULL
